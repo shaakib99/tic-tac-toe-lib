52685821ff4091484825cdb93e2a83a15112df65
Wahid Sakib <wsakib87@gmail.com>
2024-03-26 12:11:11 +0600

    updated game model to support event needs �

--- game/models.py
+++ game/models.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class GameModel(BaseModel):
     id: int
     game_id: str
-    board: list
-    turn: str
+    board: Optional[list] = None
+    turn: Optional[str] = None
     status: str
-    winner: str
-    created_at: str
-    updated_at: str
+    winner: str | None 
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes=True
 
 
 class CreateGameModel(BaseModel):
