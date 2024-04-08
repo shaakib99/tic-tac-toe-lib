c94e323e851cba2b0b194b4805df7c7257cd20ec
shaakib99 <63948199+shaakib99@users.noreply.github.com>
2024-04-09 00:39:38 +0600

    Wahid (#1)
    * updated game model to support event needs �
    
    * updated enums
    
    * added RPCException

--- common/enums.py
+++ common/enums.py
@@ -6,3 +6,19 @@ class GameStatusEnum(Enum):
     X_WON = 'X_WON'
     O_WON = 'O_WON'
     INIT = 'INIT'
+
+class ExchangeNameEnum(Enum):
+    GAME_EXCHANGE = 'game_exchange'
+    CREATE_GAME = 'create_game'
+    UPDATE_GAME = 'update_game'
+    FIND_ONE_GAME = 'find_one_game'
+    FIND_ALL_GAME  = 'find_all_game'
+
+class RoutingKeyEnum(Enum):
+    CREATE_GAME = 'create_game'
+    UPDATE_GAME = 'update_game'
+    FIND_ONE_GAME = 'find_one_game'
+    FIND_ALL_GAME  = 'find_all_game'
+
+class QueueNameEnum(Enum):
+    DEFAULT_QUEUE = 'request_queue'
