cc9150d28c22b01819ee1c1e49f7d2b919e0870b
Wahid Sakib <wsakib87@gmail.com>
2024-04-23 00:23:45 +0600

    removed user password from userModel

--- user/models.py
+++ user/models.py
@@ -5,10 +5,12 @@ from datetime import datetime
 class UserModel(BaseModel):
     id: int
     username: str
-    password: Optional[str]
     email: str
     created_at: datetime
     updated_at: datetime
+
+    class Config:
+        from_attributes=True
 
 class CreateUserModel(BaseModel):
     username: str
